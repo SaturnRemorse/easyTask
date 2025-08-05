cc8dfddcbbabfd988322b1fc1f7c3236d0720c1c
Saturn-98 <ishaan.3098@gmail.com>
2025-08-05 10:48:56 +0530

    output decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
+import { UserComponent} from "./user/user.component";
+import { DUMMY_USERS } from './dummy-user';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first-angular-app';
+  users = DUMMY_USERS;
+
+  onSelectUser(id: string){
+    console.log(id);
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,6 @@
-import { Component , computed, signal} from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, Output} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-user';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -10,17 +9,29 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => '../../assets/users/gti '+this.selectedUser().avatar);
 
-  // get imagePath(){
-  //   return "../../assets/users/"+this.selectedUser().avatar;
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string ;
+  @Input({required: true}) name!: string ;
+  @Output() select = new EventEmitter();
 
-  // }
+//   avatar = input.required<string>();
+//   name = input.required<string>();
+
+
+//   imagePath = computed(() => {
+//   return "../../assets/users/"+this.avatar();
+// });
+
+  get imagePath(){
+    return "../../assets/users/"+this.avatar;
+  }
+  
 
   onSelectUser(){
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.select.emit(this.id);
+
   }
+    
 
 }
