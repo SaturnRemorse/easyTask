296adb01ed253f29c03d738a4a7e51dc90e27395
Saturn-98 <ishaan.3098@gmail.com>
2025-08-11 10:57:27 +0530

    adding complete button functionality

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -9,7 +9,43 @@ import { TaskComponent } from "../tasks/task/task.component";
 })
 export class TasksComponent {
 
-  @Input() name?: string;
+  @Input({required: true}) userId!: string;
+  @Input({required: true}) name!: string;
+
+  tasks = [
+    {
+      id: "t1",
+      userId: "u1",
+      title: 'Master Angular',
+      summary: 'Learn all angular basic and advance features',
+      dueDate: "2025-12-31"
+    },
+    {
+      id: "t2",
+      userId: "u1",
+      title: 'Master SpringBoot',
+      summary: 'Learn all SpringBoot basic and advance features',
+      dueDate: "2025-12-31"
+    },
+    {
+      id: "t3",
+      userId: 'u3',
+      title: 'Master Docker',
+      summary: 'Learn all Docker basic and advance features',
+      dueDate: "2025-12-31"
+    }
+
+  ]
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task)=>
+      task.userId === this.userId);
+  }
+
+  onComplete(id:string){
+
+    this.tasks = this.tasks.filter((task)=>task.id !== id);
+  }
     
 
   }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,17 +1,6 @@
 import { Component, computed, EventEmitter, Input, input, output, Output} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-user';
-
-// type User = {
-//   id: string,
-//   avatar: string,
-//   name:string
-// }
-
-interface User{
-  id: string,
-    avatar: string,
-    name:string
-}
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -26,6 +15,7 @@ export class UserComponent {
   // @Input({required: true}) name!: string ;
 
   @Input({required: true}) user!: User;
+  @Input({required: true}) selected!: boolean;
   @Output() select = new EventEmitter();
   //select = output<string>();
 
